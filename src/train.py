017d52e6263e0be426db1f25ceadf10dd57d2667
asvcode <avirdee@aol.com>
2018-10-08 18:03:39 -0700

    Add files via upload

---
+++ src/train.py
@@ -0,0 +1,33 @@
+#from fastai.imports import*
+from fastai.conv_learner import *
+
+#gpu = torch.cuda.is_available()
+#print(gpu)
+
+def train():
+
+    path = 'data/pill/small'
+    labels_csv = f'{path}/classify_test_shape.csv'
+    n = len(list(open(labels_csv)))-1
+    val_idxs = get_cv_idxs(n)
+
+#aug_tfms = [RandomDihedral(), RandomLighting(0.2, 1.8)]
+
+    arch = resnet18
+    sz = 32
+    bs = 64
+#tfms = tfms_from_model(arch, sz, aug_tfms)
+    tfms = tfms_from_model(arch, sz)
+    data = ImageClassifierData.from_csv(path, 'train', labels_csv, tfms=tfms,
+                            val_idxs=val_idxs, test_name='test_one', bs=bs)
+
+    x,y=(next(iter((data.val_dl))))
+
+    learn = ConvLearner.pretrained(arch, data, ps=0.25)
+
+    wd=5e-4
+
+    learn.fit(0.01, 1, cycle_len=1, cycle_mult=2, wds=wd)
+
+if __name__ == "__main__":
+    train()
