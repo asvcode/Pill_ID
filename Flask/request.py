364faa09c8a06add86fdcb7289ce6cd34859c4a0
asvcode <avirdee@aol.com>
2018-10-11 21:35:56 -0700

    Update request.py

--- Flask/request.py
+++ Flask/request.py
@@ -2,7 +2,7 @@ import requests
 import cv2
 import json
 
-FASTAI_REST_API_URL = "http://127.0.0.1:5000/predict"
+REST_API_URL = "http://127.0.0.1:5000/predict"
 IMAGE_PATH = 'test2.jpg'
 
 content_type = 'image/jpeg'
@@ -11,6 +11,6 @@ headers = {'content_type' : content_type}
 img = cv2.imread(IMAGE_PATH)
 
 _,img_encoded = cv2.imencode('.jpg', img)
-response = requests.post(FASTAI_REST_API_URL, data=img_encoded.tostring(), headers=headers)
+response = requests.post(REST_API_URL, data=img_encoded.tostring(), headers=headers)
 
 print(json.loads(response.text))
